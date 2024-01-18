90b4829665741cb768fc9effa0076c850945b9cc
BlackCoder56 <blackcoda56@gmail.com>
2024-01-18 22:20:37 +0300

    insert into table query

--- src/mypackage/DbFuntion.java
+++ src/mypackage/DbFuntion.java
@@ -1,15 +1,17 @@
 
 package mypackage;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbFuntion {
-    public Connection connect_to_db(String db_name, String user, String pass){
-        Connection conn = null;
+    Connection conn = null;
+    public Connection connect_to_db(){
+        
         try{
             Class.forName("org.postgresql.Driver");
-            conn =  DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+db_name, user, pass);
+            conn =  DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+"crud", "postgres", "");
             if(conn!=null){
                 System.out.println("Established");
             }else{
@@ -21,8 +23,20 @@ public class DbFuntion {
         }
         
         return conn;
+    }    
+    Statement statement;
+    public void insert_data(String name, int phone, String address){
+        try{
+            Class.forName("org.postgresql.Driver");
+            conn =  DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+"crud", "postgres", "swap2");
+            String query = String.format("INSERT INTO c_table(name, phone, address) VALUES('%s', '%d', '%s');", name, phone, address);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
-    
 //    public static void main(String[] args){
 //        DbFuntion db = new DbFuntion();
 //        db.connect_to_db("crud", "postgres", "swap2");
